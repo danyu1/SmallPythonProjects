7453ab9a23a8d68c626a0277875417cd10288858
danyul <115963400+danyu1@users.noreply.github.com>
2023-07-16 16:23:48 -0700

    Add files via upload

---
+++ SpamBotPyautogui/main.py
@@ -0,0 +1,19 @@
+import pyautogui as pg
+import time
+
+time.sleep (3)
+print (pg.position ())
+#pg.moveTo (55, 164, duration=3)
+time.sleep (2)
+#pg.leftClick (55, 164, 1)
+time.sleep (2)
+#pg.leftClick (703, 803, 1)
+time.sleep (2)
+#for i in range (100):
+   # pg.typewrite ("glossy eye")
+    #pg.press("enter")
+
+
+#pg.click ()
+#pg.moveTo
+#pg.position ()
